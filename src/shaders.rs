6ab529dd0885d519c72954463b5531f7128021b3
Jules Youngberg <julesdayoungberg@gmail.com>
2021-01-02 01:29:43 -0800

    shader dependencies

--- src/shaders.rs
+++ src/shaders.rs
@@ -1,4 +1,5 @@
 use nannou::prelude::*;
+use regex::Regex;
 use shaderc;
 use std::fs;
 
@@ -18,10 +19,35 @@ pub fn compile_shader(
     let mut path = SHADERS_PATH.to_owned();
     path.push_str(filename);
 
-    // read and compile shader
+    // read shader
     let src_string = fs::read_to_string(path).expect(error.as_str());
     let src = src_string.as_str();
-    println!("{}", src);
+
+    // load shader dependencies
+    let re = Regex::new(r"\n//@import (.*)").unwrap();
+    let complete_src = re
+        .replace_all(src, |captures: &regex::Captures| {
+            let import = &captures[1];
+            println!("imported: {}", import);
+            let mut import_path = SHADERS_PATH.to_owned();
+            import_path.push_str(import);
+            import_path.push_str(".glsl");
+
+            let mut import_error = "Error reading shader '".to_owned();
+            import_error.push_str(filename);
+            import_error.push_str("' import '");
+            import_error.push_str(import);
+            import_error.push_str("'");
+
+            let mut import_src = "\n".to_owned();
+            let import_src_string = fs::read_to_string(import_path).expect(import_error.as_str());
+            import_src.push_str(import_src_string.as_str());
+            return import_src;
+        })
+        .to_string();
+    println!("{}", complete_src);
+
+    // compile shader
     let spirv = compiler
         .compile_into_spirv(src, kind, filename, "main", None)
         .unwrap();
--- src/shaders/basic.frag
+++ src/shaders/basic.frag
@@ -3,6 +3,9 @@
 layout(location = 0) in vec2 uv;
 layout(location = 0) out vec4 frag_color;
 
+//@import util
+//@import util2
+
 void main() {
     frag_color = vec4(uv, 0, 1);
 }
