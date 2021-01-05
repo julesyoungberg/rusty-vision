436cc0aa18b9373b26048e001c35166cbc30ac38
Jules Youngberg <julesdayoungberg@gmail.com>
2021-01-04 17:02:35 -0800

    vector utils

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,5 @@
 #![allow(dead_code)]
-
-use nannou::math::cgmath::Matrix3;
+use nannou::math::cgmath::Matrix4;
 use nannou::prelude::*;
 
 // The vertex type that we will use to represent a point on our triangle.
@@ -60,7 +59,7 @@ pub fn vertices_as_bytes(data: &[Vertex]) -> &[u8] {
     unsafe { wgpu::bytes::from_slice(data) }
 }
 
-pub fn rotate_around_axis(axis: Vector3, theta: f32) -> Matrix3<f32> {
+pub fn rotate_around_axis(axis: Vector3, theta: f32) -> Matrix4<f32> {
     let cos = theta.cos();
     let sin = theta.sin();
     let m00 = cos + axis.x * axis.x * (1.0 - cos);
@@ -72,5 +71,42 @@ pub fn rotate_around_axis(axis: Vector3, theta: f32) -> Matrix3<f32> {
     let m02 = axis.z * axis.x * (1.0 - cos) - axis.y * sin;
     let m12 = axis.z * axis.y * (1.0 - cos) + axis.x * sin;
     let m22 = cos + axis.z * axis.z * (1.0 - cos);
-    Matrix3::new(m00, m01, m02, m10, m11, m12, m20, m21, m22)
+    Matrix4::new(
+        m00, m01, m02, 0.0, m10, m11, m12, 0.0, m20, m21, m22, 0.0, 0.0, 0.0, 0.0, 1.0,
+    )
+}
+
+pub fn transform_vector(transform: &Matrix4<f32>, vector: Vector3) -> Vector3 {
+    let point = Point3::new(vector.x, vector.y, vector.z);
+    let transformed_point = Transform::transform_point(transform, point.into());
+    Vector3::new(
+        transformed_point.x,
+        transformed_point.y,
+        transformed_point.z,
+    )
+}
+
+pub fn vector_length(vector: Vector3) -> f32 {
+    let sum = vector.x * vector.x + vector.y * vector.y + vector.z * vector.z;
+    sum.sqrt()
+}
+
+#[cfg(test)]
+#[test]
+fn test_vector_length() {
+    assert_eq!(vector_length(pt3(1.0, 2.0, 3.0)), 3.7416575);
+}
+
+pub fn normalize_vector(vector: Vector3) -> Vector3 {
+    let len = vector_length(vector);
+    pt3(vector.x / len, vector.y / len, vector.z / len)
+}
+
+#[cfg(test)]
+#[test]
+fn test_normalize_vector() {
+    let normalized = normalize_vector(pt3(1.0, 2.0, 3.0));
+    assert_eq!(normalized.x, 0.26726124);
+    assert_eq!(normalized.y, 0.5345225);
+    assert_eq!(normalized.z, 0.8017837);
 }
