f3740cb95c3465b2b60a584976ae585150005739
Jules Youngberg <julesdayoungberg@gmail.com>
2021-01-04 19:55:34 -0800

    ui comments

--- src/interface.rs
+++ src/interface.rs
@@ -3,6 +3,9 @@ use nannou::ui::prelude::*;
 use crate::app;
 use crate::config;
 
+/**
+ * UI Components
+ */
 fn text<'a>(text: &'a str) -> widget::Text<'a> {
     widget::Text::new(text).rgb(0.1, 0.1, 0.1).font_size(12)
 }
@@ -61,10 +64,14 @@ fn unit_slider(val: f32) -> widget::Slider<'static, f32> {
         .border(0.0)
 }
 
+/**
+ * Main UI logic / layout
+ */
 pub fn update_ui(model: &mut app::Model) {
     // Calling `set_widgets` allows us to instantiate some widgets.
     let ui = &mut model.ui.set_widgets();
 
+    // main UI wrapper
     widget::BorderedRectangle::new([219.0, 500.0])
         .top_left_with_margin(10.0)
         .rgba(0.9, 0.9, 0.9, 0.7)
@@ -72,12 +79,15 @@ pub fn update_ui(model: &mut app::Model) {
         .border(1.0)
         .set(model.widget_ids.controls_rect, ui);
 
+    // hint
     text_small(&format!("Press 'h' to hide controls"))
-        .top_left_with_margin(10.0)
         .parent(model.widget_ids.controls_rect)
+        .top_left_with_margin(10.0)
         .set(model.widget_ids.toggle_controls_hint, ui);
 
+    // general controls tab
     for _click in button_big()
+        .parent(model.widget_ids.controls_rect)
         .down(10.0)
         .label("General")
         .set(model.widget_ids.general_folder, ui)
@@ -86,12 +96,18 @@ pub fn update_ui(model: &mut app::Model) {
         model.ui_show_general = !model.ui_show_general;
     }
 
+    //////////////////////////////////////////////////
+    // General Controls
+    //////////////////////////////////////////////////
     if model.ui_show_general {
+        /////////////////////////
+        // current program select
         text(&format!("Shader"))
+            .parent(model.widget_ids.controls_rect)
             .down(10.0)
             .set(model.widget_ids.current_program_label, ui);
-
         for selected in drop_down(config::PROGRAMS, model.current_program)
+            .parent(model.widget_ids.controls_rect)
             .down(5.0)
             .set(model.widget_ids.current_program, ui)
         {
@@ -101,18 +117,24 @@ pub fn update_ui(model: &mut app::Model) {
             }
         }
 
+        /////////////////////////
+        // draw floor toggle
         text(&format!("Draw Floor"))
+            .parent(model.widget_ids.controls_rect)
             .down(10.0)
             .set(model.widget_ids.draw_floor_label, ui);
-
         for _click in button_small(model.uniforms.data.draw_floor)
+            .parent(model.widget_ids.controls_rect)
             .right(110.0)
             .set(model.widget_ids.draw_floor, ui)
         {
             model.uniforms.data.draw_floor = !model.uniforms.data.draw_floor;
         }
 
+        /////////////////////////
+        // fog control
         for value in slider(model.uniforms.data.fog_dist, 15.0, 300.0)
+            .parent(model.widget_ids.controls_rect)
             .left(-30.0)
             .down(10.0)
             .label("Fog Distance")
@@ -121,7 +143,10 @@ pub fn update_ui(model: &mut app::Model) {
             model.uniforms.data.fog_dist = value;
         }
 
+        /////////////////////////
+        // quality control
         for value in slider(model.uniforms.data.quality, 1.0, 3.0)
+            .parent(model.widget_ids.controls_rect)
             .down(10.0)
             .label("Quality")
             .set(model.widget_ids.quality, ui)
@@ -129,11 +154,14 @@ pub fn update_ui(model: &mut app::Model) {
             model.uniforms.data.quality = value;
         }
 
+        /////////////////////////
+        // color mode select
         text(&format!("Color Mode"))
+            .parent(model.widget_ids.controls_rect)
             .down(10.0)
             .set(model.widget_ids.color_mode_label, ui);
-
         for selected in drop_down(config::COLOR_MODES, model.uniforms.data.color_mode as usize)
+            .parent(model.widget_ids.controls_rect)
             .down(5.0)
             .set(model.widget_ids.color_mode, ui)
         {
@@ -146,11 +174,15 @@ pub fn update_ui(model: &mut app::Model) {
         let mut right: f32;
         let step = 34.0;
 
+        /////////////////////////
+        // color 1 select
         text(&format!("Color 1"))
+            .parent(model.widget_ids.controls_rect)
             .down(10.0)
             .set(model.widget_ids.color1_label, ui);
 
         for value in unit_slider(model.uniforms.data.color1_r)
+            .parent(model.widget_ids.controls_rect)
             .rgb(0.8, 0.3, 0.3)
             .label("R")
             .set(model.widget_ids.color1_r, ui)
@@ -161,6 +193,7 @@ pub fn update_ui(model: &mut app::Model) {
         right = step;
 
         for value in unit_slider(model.uniforms.data.color1_g)
+            .parent(model.widget_ids.controls_rect)
             .rgb(0.3, 0.8, 0.3)
             .right(10.0)
             .label("G")
@@ -172,6 +205,7 @@ pub fn update_ui(model: &mut app::Model) {
         right = right + step;
 
         for value in unit_slider(model.uniforms.data.color1_b)
+            .parent(model.widget_ids.controls_rect)
             .rgb(0.3, 0.3, 0.8)
             .right(10.0)
             .label("B")
@@ -179,15 +213,18 @@ pub fn update_ui(model: &mut app::Model) {
         {
             model.uniforms.data.color1_b = value;
         }
-
         right = right + step;
 
+        /////////////////////////
+        // color 2 select
         text(&format!("Color 2"))
+            .parent(model.widget_ids.controls_rect)
             .left(right as f64)
             .down(10.0)
             .set(model.widget_ids.color2_label, ui);
 
         for value in unit_slider(model.uniforms.data.color2_r)
+            .parent(model.widget_ids.controls_rect)
             .rgb(0.8, 0.3, 0.3)
             .down(5.0)
             .label("R")
@@ -199,6 +236,7 @@ pub fn update_ui(model: &mut app::Model) {
         right = step;
 
         for value in unit_slider(model.uniforms.data.color2_g)
+            .parent(model.widget_ids.controls_rect)
             .rgb(0.3, 0.8, 0.3)
             .right(10.0)
             .label("G")
@@ -210,6 +248,7 @@ pub fn update_ui(model: &mut app::Model) {
         right = right + step;
 
         for value in unit_slider(model.uniforms.data.color2_b)
+            .parent(model.widget_ids.controls_rect)
             .rgb(0.3, 0.3, 0.8)
             .right(10.0)
             .label("B")
@@ -220,12 +259,16 @@ pub fn update_ui(model: &mut app::Model) {
 
         right = right + step;
 
+        /////////////////////////
+        // color 3 select
         text(&format!("Color 3"))
+            .parent(model.widget_ids.controls_rect)
             .left(right as f64)
             .down(10.0)
             .set(model.widget_ids.color3_label, ui);
 
         for value in unit_slider(model.uniforms.data.color3_r)
+            .parent(model.widget_ids.controls_rect)
             .rgb(0.8, 0.3, 0.3)
             .down(5.0)
             .label("R")
@@ -237,6 +280,7 @@ pub fn update_ui(model: &mut app::Model) {
         // right = 0.0;
 
         for value in unit_slider(model.uniforms.data.color3_g)
+            .parent(model.widget_ids.controls_rect)
             .rgb(0.3, 0.8, 0.3)
             .right(10.0)
             .label("G")
@@ -248,6 +292,7 @@ pub fn update_ui(model: &mut app::Model) {
         // right = right + step;
 
         for value in unit_slider(model.uniforms.data.color3_b)
+            .parent(model.widget_ids.controls_rect)
             .rgb(0.3, 0.3, 0.8)
             .right(10.0)
             .label("B")
