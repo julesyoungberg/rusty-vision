c71a2bcbee52fbc806d8dc8ba7f2479e91ac27a2
Jules Youngberg <julesdayoungberg@gmail.com>
2021-02-06 09:05:12 -0800

    move shaders out of src

--- src/app_config.rs
+++ src/app_config.rs
@@ -1,5 +1,5 @@
 pub static SIZE: [u32; 2] = [1920, 1080];
 
-pub static SHADERS_PATH: &str = "./src/shaders/";
+pub static SHADERS_PATH: &str = "./shaders/";
 
 pub const COLOR_MODES: &'static [&'static str] = &["palette", "solid"];
