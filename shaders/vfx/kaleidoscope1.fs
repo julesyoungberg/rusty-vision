e1f8aab9453f7e0b830ed0429e322186d0376e89
Jules Youngberg <julesdayoungberg@gmail.com>
2021-05-15 23:26:05 -0700

    use polling for shader file watching for vim

--- shaders/vfx/kaleidoscope1.fs
+++ shaders/vfx/kaleidoscope1.fs
@@ -7,6 +7,20 @@
         {
             "NAME": "inputImage",
             "TYPE": "image"
+        },
+        {
+            "NAME": "speed",
+            "TYPE": "float",
+            "MIN": 0.0,
+            "MAX": 0.5,
+            "DEFAULT": 0.02
+        },
+        {
+            "NAME": "slices",
+            "TYPE": "float",
+            "MIN": 1.0,
+            "MAX": 20.0,
+            "DEFAULT": 9.0
         }
     ]
 }*/
@@ -17,15 +31,15 @@
 vec2 kaleidoscope(vec2 st) {
     float a = atan(st.y, st.x);
     float r = pow(length(st), 0.9);
-    float p = sin(2.0 * PI * TIME * 0.02);
-    float q = 2.0 * PI / 9.0;
+    float p = sin(2.0 * PI * TIME * speed);
+    float q = 2.0 * PI / slices;
     a = abs(mod(a, q) - 0.5 * q);
     float factor = pow(r, 1.3) * 0.1;
     return vec2(cos(a), sin(a)) * factor;
 }
 
 vec2 transform(vec2 st) {
-    float a = TIME * 0.02;
+    float a = TIME * speed;
     vec2 v;
     v.x = st.x * cos(a) - st.y * sin(a) - 0.3 * sin(a);
     v.y = st.x * sin(a) + st.y * cos(a) + 0.3 * cos(a);
