31079142d352cdded8966136b2932df7db011598
Jules Youngberg <julesdayoungberg@gmail.com>
2021-09-18 20:51:08 -0700

    added isf parameters to hexagonal

--- shaders/patterns/hexagonal1.fs
+++ shaders/patterns/hexagonal1.fs
@@ -5,8 +5,60 @@
     "CATEGORIES": [ "GENERATOR" ],
     "INPUTS": [
         {
+            "NAME": "color_config",
+            "TYPE": "color",
+            "DEFAULT": [
+                0.4,
+                0.1,
+                1.0,
+                1.0
+            ]
+        },
+        {
             "NAME": "fft_texture",
             "TYPE": "audioFFT"
+        },
+        {
+            "NAME": "scale",
+            "TYPE": "float",
+            "MIN": 1.0,
+            "MAX": 30.0,
+            "DEFAULT": 10.0
+        },
+        {
+            "NAME": "sensitivity",
+            "TYPE": "float",
+            "MIN": 1.0,
+            "MAX": 5.0,
+            "DEFAULT": 2.0
+        },
+        {
+            "NAME": "threshold",
+            "TYPE": "float",
+            "MIN": 0.0,
+            "MAX": 1.0,
+            "DEFAULT": 0.3
+        },
+        {
+            "NAME": "color_speed",
+            "TYPE": "float",
+            "MIN": 0.0,
+            "MAX": 0.5,
+            "DEFAULT": 0.1
+        },
+        {
+            "NAME": "pulse_speed",
+            "TYPE": "float",
+            "MIN": 0.0,
+            "MAX": 5.0,
+            "DEFAULT": 1.0
+        },
+        {
+            "NAME": "frequency_density",
+            "TYPE": "float",
+            "MIN": 0.01,
+            "MAX": 1.0,
+            "DEFAULT": 0.1
         }
     ]
 }*/
@@ -15,6 +67,12 @@ vec3 hsv2rgb(vec3 c) {
     vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
     vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
     return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+// IQ's palette generator:
+// https://www.iquilezles.org/www/articles/palettes/palettes.htm
+vec3 palette(in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d) {
+    return a + b * cos(6.28318 * (c * t + d));
 }
 
 float hex_dist(in vec2 p) {
@@ -44,7 +102,7 @@ void main() {
 
     vec3 color = vec3(0);
 
-    st *= 10.0;
+    st *= scale;
 
     vec4 coords = hex_coords(st);
     vec2 gv = coords.xy;
@@ -53,14 +111,20 @@ void main() {
     float i = dot(id, id);
 
     float intensity =
-        log(IMG_NORM_PIXEL(fft_texture, vec2(fract(i * 0.1), 0)).x * 3.0 + 1.0);
+        log(IMG_NORM_PIXEL(fft_texture, vec2(fract(i * frequency_density), 0))
+                .x +
+            1.0) *
+        sensitivity;
 
-    float d = smoothstep(0.01, 0.03, gv.y * sin(i + TIME)); // * intensity * 0.5
-    // color += c;
+    float d = smoothstep(0.01, 0.03, gv.y * sin(i + TIME * pulse_speed));
 
-    color = d * hsv2rgb(vec3(sin(i + TIME * 0.1), 1, 1)).zxy *
-            log(intensity * 10.0);
-    // color = mix(vec3(0), color, smoothstep(0.05, 0.06, m_edge_dist));
+    color = palette(sin(i + TIME * color_speed), vec3(0.5, 0.5, 0.5),
+                    vec3(0.5, 0.5, 0.5), vec3(1.0, 1.0, 1.0), color_config.xyz);
+
+    color *= d * mix(0.0, 1.0, smoothstep(threshold, 1.0, intensity));
+
+    // float edge_dist = 0.5 - hex_dist(gv);
+    // color = mix(vec3(0), color, smoothstep(0.05, 0.06, edge_dist));
 
     gl_FragColor = vec4(color, 1);
 }
