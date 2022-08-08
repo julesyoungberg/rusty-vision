c29b29b37ee5abcd833785a2e6892696f0f527f0
Jules Youngberg <julesdayoungberg@gmail.com>
2022-08-07 19:10:00 -0700

    clean up noise lines and use simple fbm for other shader

--- shaders/patterns/noise_lines.fs
+++ shaders/patterns/noise_lines.fs
@@ -70,35 +70,10 @@
     ]
 }*/
 
-#define PI 3.14159265359
-
-// IQ's palette generator:
-// https://www.iquilezles.org/www/articles/palettes/palettes.htm
-vec3 palette(in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d) {
-    return a + b * cos(6.28318 * (c * t + d));
-}
-
-float noise_hash2(vec2 p) {
-    p = 50.0 * fract(p * 0.3183099 + vec2(0.71, 0.113));
-    return -1.0 + 2.0 * fract(p.x * p.y * (p.x + p.y));
-}
-
 float noise_hash3(vec3 p) {
     p = fract(p * 0.3183099 + .1);
     p *= 17.0;
     return fract(p.x * p.y * p.z * (p.x + p.y + p.z));
-}
-
-float noise21(in vec2 p) {
-    vec2 i = floor(p);
-    vec2 f = fract(p);
-    vec2 u = f * f * (3.0 - 2.0 * f);
-
-    return mix(mix(noise_hash2(i + vec2(0.0, 0.0)),
-                   noise_hash2(i + vec2(1.0, 0.0)), u.x),
-               mix(noise_hash2(i + vec2(0.0, 1.0)),
-                   noise_hash2(i + vec2(1.0, 1.0)), u.x),
-               u.y);
 }
 
 float noise31(in vec3 x) {
@@ -135,7 +110,6 @@ float fbm(in vec2 p) {
     f /= scaling;
     return f;
 }
-
 
 float line_dist(vec2 p, vec2 a, vec2 b) {
     vec2 pa = p - a;
